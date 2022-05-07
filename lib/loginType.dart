c9f3578d9ac5a8736e443c6ecc97b69552ffecb7
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-05-07 02:11:59 +0200

    Update loginType.dart

--- lib/loginType.dart
+++ lib/loginType.dart
@@ -11,7 +11,7 @@ class loginAs extends StatelessWidget {
         decoration: BoxDecoration(
             gradient: LinearGradient(
                 colors: [Color(0xffE822AC), Color(0xff9E32C6)],
-                stops: [0.6, 2],
+                // stops: [0.6, 2],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter)),
       ),
