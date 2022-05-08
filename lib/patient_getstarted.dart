3b8f21afabdb6bf8291aaa34861197d9a8efbd50
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-05-08 13:49:56 +0200

    Update patient_getstarted.dart

--- lib/patient_getstarted.dart
+++ lib/patient_getstarted.dart
@@ -19,58 +19,59 @@ class getstarted_patient extends StatelessWidget {
                   image: DecorationImage(
                       image: AssetImage('assets/images/getstarted.png'),
                       fit: BoxFit.fill))),
-          Column(
-            children: [
-              const SizedBox(
-                height: 35,
-              ),
-              Image.asset('assets/images/LogoWhite.png'),
-              const SizedBox(
-                height: 225,
-              ),
-              Column(
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            CustomPaint(
+              size: Size(
+                  double.infinity,
+                  (300 * 0.657932839159359)
+                      .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+              painter: WavyGetStarted(),
+            ),
+          ]),
+          Column(children: [
+            const SizedBox(
+              height: 35,
+            ),
+            Image.asset('assets/images/LogoWhite.png'),
+            const SizedBox(
+              height: 225,
+            )
+          ]),
+          Container(
+            height: 350,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end,
 // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    const CustomText(
-                      text: 'Great',
+                children: [
+                  const CustomText(
+                    text: 'Great',
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28.0,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  const Divider(
+                    color: Color(0xffFEB197),
+                    indent: 175,
+                    endIndent: 175,
+                    height: 30,
+                    thickness: 2,
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: CustomText(
+                      text:
+                          'The point of using Lorem Ipsum is that\n it has a more-or-less no distribution of\n \t\t\t\t\t\t\t\t\t\tlook like readable english.',
                       textStyle: TextStyle(
                         color: Colors.white,
-                        fontSize: 28.0,
+                        fontSize: 17.0,
                         fontFamily: 'Inter',
-                        fontWeight: FontWeight.w800,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                    const Divider(
-                      color: Color(0xffFEB197),
-                      indent: 175,
-                      endIndent: 175,
-                      height: 30,
-                      thickness: 2,
-                    ),
-                    const Align(
-                      alignment: Alignment.center,
-                      child: CustomText(
-                        text:
-                            'The point of using Lorem Ipsum is that\n it has a more-or-less no distribution of\n \t\t\t\t\t\t\t\t\t\tlook like readable english.',
-                        textStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17.0,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ]),
-              Column(children: [
-                CustomPaint(
-                  size: Size(
-                      double.infinity,
-                      (306 * 0.657932839159359)
-                          .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-                  painter: WavyGetStarted(),
-                ),
-              ]),
-            ],
+                  ),
+                ]),
           ),
         ],
       ),
