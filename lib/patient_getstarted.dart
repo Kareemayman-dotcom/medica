90bca8aa6900ba731fa4a17b350dab3b56155954
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-05-08 13:22:31 +0200

    Update patient_getstarted.dart

--- lib/patient_getstarted.dart
+++ lib/patient_getstarted.dart
@@ -4,9 +4,8 @@ import 'package:medica/view/widgets/custom_text.dart';
 import 'package:medica/view/widgets/wavey_shape.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-
 class getstarted_patient extends StatelessWidget {
-  const  getstarted_patient ({ Key? key }) : super(key: key);
+  const getstarted_patient({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +19,7 @@ class getstarted_patient extends StatelessWidget {
                   image: DecorationImage(
                       image: AssetImage('assets/images/getstarted.png'),
                       fit: BoxFit.fill))),
-                      Column(
+          Column(
             children: [
               const SizedBox(
                 height: 35,
@@ -29,64 +28,52 @@ class getstarted_patient extends StatelessWidget {
               const SizedBox(
                 height: 225,
               ),
-
-Column(
+              Column(
 // ignore: prefer_const_literals_to_create_immutables
-children: [
-const CustomText(
-                text: 'Great',
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28.0,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-                const Divider(
-                        color: Color(0xffFEB197),
-                        indent: 175,
-                        endIndent: 175,
-                        height: 30,
-                        thickness: 2,
+                  children: [
+                    const CustomText(
+                      text: 'Great',
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w800,
                       ),
-            
-          const Align(alignment: Alignment.center,
-          
-         child: CustomText(
-                text: 'The point of using Lorem Ipsum is that\n it has a more-or-less no distribution of\n \t\t\t\t\t\t\t\t\t\tlook like readable english.',
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17.0,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w400,
-                  
+                    ),
+                    const Divider(
+                      color: Color(0xffFEB197),
+                      indent: 175,
+                      endIndent: 175,
+                      height: 30,
+                      thickness: 2,
+                    ),
+                    const Align(
+                      alignment: Alignment.center,
+                      child: CustomText(
+                        text:
+                            'The point of using Lorem Ipsum is that\n it has a more-or-less no distribution of\n \t\t\t\t\t\t\t\t\t\tlook like readable english.',
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17.0,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ]),
+              Column(children: [
+                CustomPaint(
+                  size: Size(
+                      double.infinity,
+                      (306 * 0.657932839159359)
+                          .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                  painter: WavyGetStarted(),
                 ),
-              ),
-          
-          ),
-            ]
-
-),
-
-
-    Column(
-           
-            children: [
-              CustomPaint(
-                 size: Size(
-                     double.infinity,
-                     (306 * 0.657932839159359)
-                         .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-                 painter: WavyGetStarted(),
-                  ),
-         
-                ]
-              ),
+              ]),
             ],
           ),
         ],
       ),
     );
-    
   }
 }
