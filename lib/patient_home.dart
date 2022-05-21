ed0f6cd923cf5abc79791e4340b381d42f9c5d29
Ahmed Nabil Mahran <ahmednabilmahran@gmail.com>
2022-05-21 18:42:36 +0200

    Save User Data (Modified)

---
+++ lib/patient_home.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors, unused_import, unnecessary_import, import_of_legacy_library_into_null_safe
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:medica/patient%20_login.dart';
+import 'package:medica/patient_register.dart';
+import 'package:medica/view/widgets/LnRCurve.dart';
+import 'package:medica/view/widgets/constance.dart';
+import 'package:medica/view/widgets/custom_background.dart';
+import 'package:medica/view/widgets/custom_text.dart';
+import 'package:medica/view/widgets/wavey_shape.dart';
+import 'package:medica/core/view_model/auth_view_model.dart';
+
+class patient_home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return WillPopScope(
+      onWillPop: () async {
+        // Get.to(loginAs());
+        Get.back();
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            myDefaultBackground(),
+            Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/png/register.png'),
+                        fit: BoxFit.fill))),
+
+                        Container(
+              // padding: EdgeInsets.symmetric(
+              //   horizontal: size.width * 0.05,
+              // ),
+              // color: Colors.green,
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: size.height * 0.06,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                    SizedBox(
+                    width: size.width * 0.09,
+                  ),
+                    Image.asset('assets/images/Menu.png'),
+                    SizedBox(
+                    width: size.width * 0.09,
+                  ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Stack(alignment: Alignment.bottomCenter, children: [
+              Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                CustomPaint(
+                  size: Size(
+                      double.infinity,
+                      (size.height * 0.45 * 1.9280575539568345)
+                          .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                  painter: LnRCurve(),
+                ),
+              ]),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
