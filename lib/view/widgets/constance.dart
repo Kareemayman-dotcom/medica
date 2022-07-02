18873db02cb486a50bf08d66b4e85e01150d6e02
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-07-02 20:34:28 +0200

    Merge branch 'main' of https://github.com/Kareemayman-dotcom/medica

--- lib/view/widgets/constance.dart
+++ lib/view/widgets/constance.dart
@@ -9,6 +9,7 @@ const Color primaryColorShadow = Color.fromARGB(19, 109, 5, 255);
 const Color primaryColorOutOfFocus = Color.fromARGB(255, 127, 112, 167);
 const Color secondaryColor = Color(0xffF95DDE);
 const Color linkColor = Color(0xff2CC1BE);
+const Color medicaCyan = Color(0xff4CD2CF);
 const Color secondaryTextColor = Color(0xff8C849F);
 const k_keepMeLoggedIn = 'KeepMeLoggedIn';
 String current_user = patient_home() as String;
